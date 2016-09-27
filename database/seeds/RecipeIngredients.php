2c75bdf6db1663c56e5b69fa922d9c13679e4f51
ccyd6c <ccyd6c@mail.missouri.edu>
2016-09-27 09:21:11 -0500

    finished adding recipe ingredients

--- database/seeds/RecipeIngredients.php
+++ database/seeds/RecipeIngredients.php
@@ -32,5 +32,110 @@ class RecipeIngredients extends Seeder
             'recipe_id' => '2',
             'quantity'=> '.1',
         ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '4',
+            'recipe_id' => '2',
+            'quantity'=> '4',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '13',
+            'recipe_id' => '1',
+            'quantity'=> '8',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '14',
+            'recipe_id' => '1',
+            'quantity'=> '5',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '7',
+            'recipe_id' => '3',
+            'quantity'=> '1',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '8',
+            'recipe_id' => '3',
+            'quantity'=> '1',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '9',
+            'recipe_id' => '3',
+            'quantity'=> '1',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '11',
+            'recipe_id' => '3',
+            'quantity'=> '1',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '12',
+            'recipe_id' => '3',
+            'quantity'=> '1',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '15',
+            'recipe_id' => '4',
+            'quantity'=> '1',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '8',
+            'recipe_id' => '4',
+            'quantity'=> '1',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '15',
+            'recipe_id' => '4',
+            'quantity'=> '1',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '16',
+            'recipe_id' => '4',
+            'quantity'=> '1',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '1',
+            'recipe_id' => '5',
+            'quantity'=> '1',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '16',
+            'recipe_id' => '5',
+            'quantity'=> '1',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '8',
+            'recipe_id' => '5',
+            'quantity'=> '2',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '9',
+            'recipe_id' => '6',
+            'quantity'=> '4',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '17',
+            'recipe_id' => '6',
+            'quantity'=> '4',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '18',
+            'recipe_id' => '6',
+            'quantity'=> '5',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '19',
+            'recipe_id' => '6',
+            'quantity'=> '2.5',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '12',
+            'recipe_id' => '6',
+            'quantity'=> '5',
+        ]);
+        DB::table('recipe_ingredients')->insert([
+            'item_id' => '4',
+            'recipe_id' => '6',
+            'quantity'=> '8',
+        ]);
     }
 }
--- database/seeds/RecipeTable.php
+++ database/seeds/RecipeTable.php
@@ -31,5 +31,31 @@ class RecipeTable extends Seeder
             'prep_time' => '15',
 
         ]);
+        DB::table('recipes')->insert([
+            'id' => '1',
+            'name' => 'Cheese Burger',
+            'instructions' => 'Preheat grill to at least 500 degrees. Create beef patty from 1/4lb beef while waiting. Season to taste. Place beef patty on grill wait for 5 minutes. While cooking prepair other ingredients. After 5 minutes filp burger and cook 3 minutes. Remove from grill and    immediatly place cheese on patty to let it melt. Place patty on bun with the rest of the ingredients on top. Enjoy',
+            'prep_time' => '15',
+        ]);
+        DB::table('recipes')->insert([
+            'id' => '1',
+            'name' => 'Turkey Sandwich',
+            'instructions' => 'Place slices of bread on plate. Add cheese to one slice. Place meat on the other slice. Combine and enjoy!',
+            'prep_time' => '5',
+        ]);
+        DB::table('recipes')->insert([
+            'id' => '1',
+            'name' => 'Grilled Cheese',
+            'instructions' => 'Spread 1/2 tablespoon of butter on one side of each slice of bread. Place cheese on un buttered side. Place slices together with butter facing outward. Heat skillet to medium heat. Place sandwhich on skillet, cook on each side for 3 minutes to melt cheese. Enjoy!',
+            'prep_time' => '10',
+        ]);
+        DB::table('recipes')->insert([
+            'id' => '1',
+            'name' => 'Tacos',
+            'instructions' => '"Cook beef in 10-inch skillet over medium heat 8 to 10 minutes, stirring occasionally, until brown; drain.
+            Stir salsa into beef. Heat to boiling, stirring constantly; reduce heat to medium-low. Cook 5 minutes, stirring occasionally. Pour beef mixture into large serving bowl.
+            Heat taco shells as directed on package. Serve taco shells with beef mixture, lettuce, tomato and cheese."',
+            'prep_time' => '20',
+        ]);
     }
 }
