090835a728ac4e52285323bdb6ed16502f1204d5
cwm262 <azathoth.hp@gmail.com>
2016-10-11 01:47:26 +0000

    Added missing relationship to inventory model. Corrected syntax error in itemcontroller.

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -29,28 +29,7 @@ class ItemController extends Controller
      */
     public function store(Request $request)
     {
-        try{
-            $item = new Item;
-
-            $item->item_id = $request->('item_id');
-            $item->barcode = $request->('barcode');
-            $item->user_id = $request->('user_id');
-            $item->item_name = $request->('item_name');
-            $item->measurement = $request->('measurement');
-            $item->serving_sive = $request->('serving_size');
-            $item->servings_per_container = $request->('servings_per_container');
-            $item->type = $request->('type');
-            $item->storage = $request->('storage');
-            $item->expiration = $request->('expiration');
-            $item->ready_to_eat = $request->('ready_to_eat');
-            $item->created_at = $request->('created_at');
-            $item->updated_at = $request->('updated_at');
-
-            $item->save();
-        }catch(\Exception $e){
-            Log::critical($e->getMessage());
-            return response()->json(array('message' => "Contact support with time that error occurred."), 500);
-        }
+        
     }
 
     /**
