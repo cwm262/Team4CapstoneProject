9168d9164ad4dc2cdec5f35a17b8a196c4a11e37
acbgf8 <acbgf8@mail.missouri.edu>
2016-10-26 20:14:23 +0000

    start on the recipe suggestion and get basic logic implemented

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -16,6 +16,39 @@ class NotificationController extends Controller
     }
 
     public function recipes($user_id){
+
+        try{
+           $recipes = recipes::where('user_id', $user_id)->orderBy('name', 'asc')->get();
+           $invetoryItems = inventory::where('user_id', $user_id)->orderBy('name', 'asc')->get();
+
+            foreach($recipes as $recipe) {
+                $key = $recipe->recipie_id;
+                $ingredients = recipe_ingredients::where('recipe_id', $key)->get();
+                $ingredientCount = count($ingredients);
+
+                foreach($ingredients as $ingredient) {
+                    $recipeItem = $ingredient->item_id;
+
+                    foreach ($inventoryItems as $inventoryItem){
+                        if($inventoryItem->item_id = $recipeItem){
+                            $count++;
+                        }
+                    }
+                }
+                if ($ingredientCount = $count){
+                        $haveIngredients += $recipe;
+                    }
+            }
+
+
+            return response()->json($haveIngredients);
+
+            
+        }
+        catch(\Exception $e){
+            Log::critical($e->getMessage());
+            return response()->json(array('message' => "Contact support with time that error occurred."), 500);
+        }
         //Create an algorithm/query to build a list of recipe suggestions
         //Return json
     }
