bd5b61e6a271aa57fee2cd39d1cd1cad0289d825
cwm262 <azathoth.hp@gmail.com>
2016-11-08 22:41:01 -0600

    Changed stats to show as pie charts

--- public/js/controllers/stats.controller.js
+++ public/js/controllers/stats.controller.js
@@ -12,18 +12,31 @@
         vm.data = [];
 
         vm.labels = [
-            moment().format('MMMM'),
-            moment().subtract(1, 'months').format('MMMM'),
-            moment().subtract(2, 'months').format('MMMM'),
-            moment().subtract(3, 'months').format('MMMM'),
-            moment().subtract(4, 'months').format('MMMM'),
-            moment().subtract(5, 'months').format('MMMM')
+            "Wasted",
+            "Used"
+            //moment().format('MMMM')
+            // moment().subtract(1, 'months').format('MMMM'),
+            // moment().subtract(2, 'months').format('MMMM'),
+            // moment().subtract(3, 'months').format('MMMM'),
+            // moment().subtract(4, 'months').format('MMMM'),
+            // moment().subtract(5, 'months').format('MMMM')
         ];
         
-        vm.series = ['Food Waste'];
+        //vm.series = ['Food Waste'];
 
         stats.getAll(USER_ID).then(function(response){
-            vm.data = response['Waste Per Month'];
+            vm.month0 = response['month 0'];
+            vm.month0.shift();
+            vm.month1 = response['month 1'];
+            vm.month1.shift();
+            vm.month2 = response['month 2'];
+            vm.month2.shift();
+            vm.month3 = response['month 3'];
+            vm.month3.shift();
+            vm.month4 = response['month 4'];
+            vm.month4.shift();
+            vm.month5 = response['month 5'];
+            vm.month5.shift();
         })
     }
 })();
